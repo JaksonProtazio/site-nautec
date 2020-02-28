91f1777f07b91b8bde413e1913638aa991ad10dd
JaksonFives <jpv.lic16@uea.edu.br>
2020-02-28 14:54:18 -0400

    Fonte alterada

--- resources/views/_includes/topo-index.blade.php
+++ resources/views/_includes/topo-index.blade.php
@@ -8,7 +8,9 @@
     <title>@yield('titulo')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/estilo.css')}}">
+    
 </head>
 
 <body class="d-flex flex-column h-100">
